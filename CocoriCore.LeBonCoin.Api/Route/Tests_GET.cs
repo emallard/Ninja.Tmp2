36be11b8b3d29b75ead55fdf925f178f26c0ae17
etienne <etienne.mallard@gmail.com>
2019-09-16 09:00:43 +0200

    TestTree

--- CocoriCore.LeBonCoin.Api/Route/Tests_GET.cs
+++ CocoriCore.LeBonCoin.Api/Route/Tests_GET.cs
@@ -1,37 +1,84 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CocoriCore.LeBonCoin.Api
 {
-    public class Tests_GET : IMessage<Tests_Response[]>
+    public class Tests_GET : IMessage<TestTree>
     {
 
     }
 
-    public class Tests_Response
+    public class TestItem
     {
         public Type Type;
         public string ClassName;
         public string TestName;
     }
 
-    public class Tests_GETHandler : MessageHandler<Tests_GET, Tests_Response[]>
+    public class TestNode
     {
-        public override async Task<Tests_Response[]> ExecuteAsync(Tests_GET message)
+        public string Name;
+        public List<TestNode> Children = new List<TestNode>();
+        public List<TestItem> Tests = new List<TestItem>();
+    }
+
+    public class TestTree
+    {
+        public TestItem[] Items;
+        public TestNode RootNode;
+        public void Init(TestItem[] tests)
+        {
+            Items = tests;
+            RootNode = new TestNode() { Name = "" };
+            foreach (var t in tests)
+                AddTest(t);
+        }
+
+        private void AddTest(TestItem test)
+        {
+            var parts = test.Type.FullName.Split(".");
+            var currentNode = RootNode;
+            for (var i = 0; i < parts.Length; ++i)
+            {
+                var part = parts[i];
+                var found = currentNode.Children.FirstOrDefault(x => x.Name == part);
+                if (found != null)
+                    currentNode = found;
+                else
+                {
+                    var newNode = new TestNode();
+                    newNode.Name = part;
+                    currentNode.Children.Add(newNode);
+                    currentNode = newNode;
+                }
+            }
+
+            currentNode.Tests.Add(test);
+        }
+    }
+
+    public class Tests_GETHandler : MessageHandler<Tests_GET, TestTree>
+    {
+        public override async Task<TestTree> ExecuteAsync(Tests_GET message)
         {
             await Task.CompletedTask;
             var assembly = CocoriCore.LeBonCoin.AssemblyInfo.Assembly;
-            return assembly.GetTypes().SelectMany(t =>
+            var items = assembly.GetTypes().SelectMany(t =>
             {
                 var methods = t.GetMethods().Where(m => m.GetCustomAttributes(typeof(Xunit.FactAttribute), false).Length > 0).ToArray();
-                return methods.Select(m => new Tests_Response
+                return methods.Select(m => new TestItem
                 {
                     Type = t,
                     ClassName = t.Name,
                     TestName = m.Name
                 }).ToArray();
             }).ToArray();
+
+            var tree = new TestTree();
+            tree.Init(items);
+            return tree;
         }
     }
 }
